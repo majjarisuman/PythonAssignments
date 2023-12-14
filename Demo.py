be61aa819594b2ba7590161982d46348ab3e4718
majjarisuman <suman8.marolix@gmail.com>
2023-12-14 22:10:11 +0530

    String Concatenate

--- Demo.py
+++ Demo.py
@@ -1,2 +1,15 @@
 #print statement.
 print("Hello World.!")
+
+#Python string Concatenate 
+name1 = "Rohit"
+name2 = "Sharma"
+fullName = name1+" "+name2
+print(fullName)
+
+#sum of two numers
+x = 15
+y = 25
+z = x+y
+print(z)
+
