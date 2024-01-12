6d77bf0e01e6e43888b615640807b3aafabaf1f9
majjarisuman <suman8.marolix@gmail.com>
2024-01-12 17:12:53 +0530

    Slice and typecast assignment

---
+++ 10-1-24-Assignment-Slice-Typecast/practice.py
@@ -0,0 +1,21 @@
+a = "Python Basics"
+
+# print(a[ : : ])
+# print(a[::-1])
+# print(a[::2])
+# print(a[3:12:3])
+# print(a[-1:-10:5])
+# print(a[-3:-10:-5])
+# print(a[13])
+# print(a[-14])
+# print(a[5:19:1])
+# print(a[5:19:2])
+# print(a[-8:-14])
+# print(a[-8:-14:-1])
+# print(a[-1:-7:-1])
+# print(a[-13:-7:-2 ])
+
+t1 = (33,22,45,11,4)
+print(t1)
+print(t1)
+
