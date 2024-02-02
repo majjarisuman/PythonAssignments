08948a5e5e1943975757a4aeed598b8cc13ff714
majjarisuman <153828439+majjarisuman@users.noreply.github.com>
2024-02-02 10:33:09 +0530

    Delete 2-2-24-StringMethods directory

--- 2-2-24-StringMethods/substring_in_mainstring.py
+++
@@ -1,9 +0,0 @@
-Main_string = input("Enter a sentence : ")
-Substring = input("Enter a substring : ")
-
-index = Main_string.find(Substring)
-if index != -1:
-    print(f"substring {Substring} is present in the Mainstring at index {index}")
-else:
-    print(f"Substring {Substring} not found in the Mainstring")
-
